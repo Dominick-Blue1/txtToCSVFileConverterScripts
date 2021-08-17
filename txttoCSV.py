0671f533eed3b1f309e75d6179d13364c2c109e6
Ops-Center <dblue@Ops-Centers-MacBook-Pro.local>
2021-08-16 14:58:02 -1000

    Initial commit - this is to log the text -> CSV scripts.

---
+++ txttoCSV.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#  This script takes in one text file and converts to a CSV.
+#  read_file = pd.read_csv(r'insert the pathway to the text file here/sample1.txt')
+#  read_file.to_csv(r'insert desired pathway to new CSV file location here/newCSV.csv', index=None)
