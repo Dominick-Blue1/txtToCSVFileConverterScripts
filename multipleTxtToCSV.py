7750f34bb992e038eb1da65d777e584fc04696f1
Dominick Blue <dominickblue@meshgrid.us>
2021-08-17 17:03:22 -1000

    Building script to iterate over multiple text files within directory. Will work on changing extension from ".txt" to ".csv" tomorrow.

--- multipleTxtToCSV.py
+++ multipleTxtToCSV.py
@@ -7,11 +7,12 @@ import pandas as pd
 dirname = '/Users/dblue/PycharmProjects/hmg3/txtToCSVFileConverter/batchTextFiles/7-21-Planned-Solar-Outage'
 
 # giving file extension
-ext = ('.txt')
+ext = '.txt'
 
 # iterating over all files
 for files in os.listdir(dirname):
     if files.endswith(ext):
         print(files)  # printing file name of desired extension
+
     else:
         continue
